3521276c925b861328448c49655d9a413c92522d
Luis Mochan <mochan@fis.unam.mx>
2017-08-03 20:16:57 -0500

    el orden de los campos es ahora arbitrario

--- articulos2yaml.pl
+++ articulos2yaml.pl
@@ -9,37 +9,39 @@ use open qw/ :std :encoding(utf8) /;
 
 my $yaml=YAML::Tiny->new();
 my $articulos;
+my $formato;
 while(<>){
     $articulos.=$_ if(/^\.Artículos/../^\.FinArtículos/);
+    $formato=$_ if /.Formato/;
 }
+
+$formato=substr extract_bracketed($formato,'[]','(?ms:.*?)(?=\[)'),1,-1;
+my @campos;
+while(my $campo=
+      extract_bracketed($formato,'[]','(?ms:.*?)(?=\[)')) {
+    push @campos, substr $campo, 1, -1;
+}
+
 my @articulos;
 while(my $articulo=extract_bracketed($articulos,'[]','(?ms:.*?)(?=\[)')){
     push @articulos, $articulo;
 }
 
-foreach(@articulos){
-    my %struct;
+foreach(@articulos) {
+    my %struct ;
     s/\s+/ /gms; #quita cambios de línea y junta espacios en 1.
     $_=substr($_, 1,-1);
-    my $titulo=extract_bracketed($_,'[]','(.*?)(?=\[)');
-    $titulo=~s/^\[\s*(.*)\s*\]$/$1/;
-    $struct{título}=$titulo;
-    $struct{autores}=autores(
-	substr extract_bracketed($_,'[]','(.*?)(?=\[)'),1,-1
-	);
-    my $revista=extract_bracketed($_,'[]','(.*?)(?=\[)');
-    $revista=~s/^\[\s*(.*)\s*\]$/$1/;
-    $struct{revista}=$revista;
-    my $volumen=extract_bracketed($_,'[]','(.*?)(?=\[)');
-    $volumen=~s/^\[\s*(.*)\s*\]$/$1/;
-    $struct{volumen}=$volumen;
-    my $paginas=extract_bracketed($_,'[]','(.*?)(?=\[)');
-    $paginas=~s/^\[\s*(.*)\s*\]$/$1/;
-    $struct{páginas}=$paginas;
-    my $anho=extract_bracketed($_,'[]','(.*?)(?=\[)');
-    $anho=~s/^\[\s*(.*)\s*\]$/$1/;
-    $struct{año}=$anho;
-    push @{$yaml->[0]->{"artículos"}}, \%struct;
+    foreach my $campo(@campos) {
+	if($campo=~/autores/){
+	    $struct{$campo}=autores(
+		substr extract_bracketed($_,'[]','(.*?)(?=\[)'),1,-1
+		);
+	} else {
+	    $struct{$campo}=extract_bracketed($_,'[]','(.*?)(?=\[)');
+	    $struct{$campo}=~s/^\[\s*(.*)\s*\]$/$1/;
+	}
+    }
+push @{$yaml->[0]->{"artículos"}}, \%struct;
 }
 
 my $out=$yaml->write_string();
